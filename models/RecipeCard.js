d4dd788178a5b94b8b9a8cd318503e1d4391d218
sensologica <119978540+sensologica@users.noreply.github.com>
2023-07-11 10:51:39 +0200

    Remove unused properties from Recipe Card

--- models/RecipeCard.js
+++ models/RecipeCard.js
@@ -1,24 +1,16 @@
 export default class RecipeCard {
-  #id;
   #image;
   #name;
-  #servings;
   #ingredients;
   #time;
   #description;
-  #appliance;
-  #utensils;
 
   constructor(data) {
-   this.#id = data.id;
    this.#image = data.image;
    this.#name = data.name;
-   this.#servings = data.servings;
    this.#ingredients = data.ingredients;
    this.#time = data.time;
    this.#description = data.description;
-   this.#appliance = data.appliance;
-   this.#utensils = data.utensils;
   }
 
   get image() {
