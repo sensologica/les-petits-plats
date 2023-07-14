a3aba95bc692d351826950bcb03b23ff88e5ecc0
sensologica <119978540+sensologica@users.noreply.github.com>
2023-07-14 11:29:33 +0200

    ADD: Diplay "Recette" or "Recettes" depending on the number of Recipe matches

--- models/RecipeCounter.js
+++ models/RecipeCounter.js
@@ -16,7 +16,7 @@ export default class RecipeCounter {
   render() {
     const counter = document.createElement("div");
     counter.classList.add("recipe-counter");
-    counter.innerText = `${this.count} recettes`;
+    counter.innerText = ( this.count === 1 ? `${this.count} recette` : `${this.count} recettes` );
     
     const toolbar = document.querySelector(".toolbar__recipe-counter");
     toolbar.innerHTML = "";
